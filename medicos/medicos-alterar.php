fc310ce5c6b9f56c4485bd6840668b8a886462c1
joaopedrobordin <bpj7@hotmail.com>
2021-05-16 00:30:18 -0300

    delet e insert

---
+++ medicos/medicos-alterar.php
@@ -0,0 +1,20 @@
+<?php 
+include "../include/conexao.php";
+
+     $id_medico = $_POST['id_medico'];
+     $nome = $_POST['nome'];
+     $telefone = $_POST['telefone'];
+     $crm = $_POST['crm'];
+     $especialidade = $_POST['especialidade'];
+
+$sqlAlterar = "UPDATE tb_medicos SET nome = '{$nome}' , telefone = '{$telefone}' , crm = '{$crm}' , especialidade = '{$especialidade}' WHERE id = {$id_medico}";
+
+$resultado = mysqli_query($conexao , $sqlAlterar);
+
+if($resultado){
+    echo "Alterado com sucesso!<br>";
+    echo "<a href='medicos-listar.php'>VOLTAR</a>";
+}
+else{
+    echo "Deu errado";
+} ?>
--- medicos/medicos-formulario-inserir.php
+++ medicos/medicos-formulario-inserir.php
@@ -1,24 +1,42 @@
-<?PHP include "../include/cabecalho.php" ; ?>
+<?PHP include "../include/cabecalho.php" ; 
 
-<form nome="formulario-inserir-medicos" method="post" action="medicos-inserir.php">
+$id_medico = $_GET['id_medico'] 
+
+include "../include/conexao"; 
+$sqlBuscar = 'SELECT * FROM tb_medicos WHERE id = {$id_medico};';
+
+$lista_medicos = mysqli_query($conexao , $sqlBuscar);
+
+$nome = $telefone = $crm = $especialidade = ""
+
+while($medico = msqli_fetch_assoc($lista_medicos))
+    {$nome = $medico['nome'];
+     $telefone = $medico['telefone'];
+     $crm = $medico['crm'];
+     $especialide = $medicos['especialidade'];}
+?>
+
+<form nome="formulario-inserir-medicos" method="post" action="medicos-alterar.php">
+
+<input name="id_medico" type="hidden"value="<?php echo $_GET['id_medico']; ?>">
     <p>
-    <label>Nome:</lebel>    <input name="nome"> 
+    <label>Nome:</lebel>    <input name="nome" values="<?php echo $nome; ?>"> 
     </p>
 
     <p>
-    <label>telefone:</label>    <input name="telefone">   
+    <label>telefone:</label>    <input name="telefone" values="<?php echo $telefone; ?>">   
     </p>
 
     <p>
-    <label>crm</label>     <input name="crm">
+    <label>crm</label>     <input name="crm" values="<?php echo $crm; ?>">
     </p>
 
     <p>
     <label>Especialidade:</label>
     <select name="especialidade">
-         <option values="cardiologista">cardiologista</option>   
-         <option values="nutricionista">nutricionista</option>
-         <option values="ortopedista">ortopedista</option>
+         <option value="cardiologista"<?php if($especialidade == 'cardiologista'){ echo "selected "; } ?>>cardiologista</option>   
+         <option value="nutricionista"<?php if($especialidade == 'nutricionista'){ echo "selected "; } ?>>nutricionista</option>
+         <option value="ortopedista"<?php if($especialidade == 'ortopedista'){ echo "selected "; } ?>>ortopedista</option>
     </select>
     </p>
 
